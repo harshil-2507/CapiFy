98f0fdb0ab77aad03bbf8e8d22b5b387bd64f0b7
Deep Das <deepdblm@gmail.com>
2024-07-30 19:05:41 +0530

    made responsive all react components

--- news section/src/components/navbar.jsx
+++ news section/src/components/navbar.jsx
@@ -3,8 +3,7 @@ const Navbar = ({setCategory}) => {
         
             <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
                 <div className="container-fluid">
-                    {/* <div className="navbar-brand"><span className="badge bg-light text-dark fs-4">CapiFy</span></div>  */}
-                    {/* you can also use png of your logo than span tag */}
+
                     <div className="navbar-brand text-centerfs-4" ><span className="badge text-center bg-light text-dark fs-4"> CapiFy </span></div>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
